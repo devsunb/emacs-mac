a5c2eaa91e63708c6587d84d7f400fa591fe2ca8
Jaeseok Lee <devsunb@gmail.com>
2026-07-11 16:21:15 +0900

    Org 수식 이미지의 두 배율을 모두 보존한다
    org-create-formula-image의 1x와 2x 변환이 같은 임시 출력 경로를
    쓰므로 1x 결과를 복사하기 전에 2x 변환을 돌리면 덮어써 두 목적지
    모두 고해상도 이미지를 받는다. 비-Retina 화면에서 수식이 두 배로
    보인다.
    
    1x를 먼저 복사한 뒤 2x를 변환하고 복사한다. 테스트는
    org-compile-file을 스텁으로 바꿔 두 목적지가 각자 배율의 결과를
    받는지 본다.
    
    org.el은 org 저장소에서 동기화되는 파일이라 머지 충돌 확률이 높다.

--- test/lisp/org/org-tests.el
+++ test/lisp/org/org-tests.el
@@ -23,11 +23,63 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+(require 'org)
+
 (ert-deftest org-package-version ()
   "Test Version: header is present and correct.
 Ref <https://debbugs.gnu.org/30310>."
   (should (require 'org-version nil t))
   (should (equal (version-to-list (org-release))
                  (cdr (assq 'org package--builtin-versions)))))
+
+(ert-deftest org-create-formula-image-keeps-separate-1x-and-2x-results ()
+  "Test that the 1x and 2x files each get their own conversion's output."
+  (let* ((directory (make-temp-file "org-formula-image-" t))
+         (temporary-file-directory directory)
+         (tofile (expand-file-name "formula.png" directory))
+         (to2xfile (expand-file-name "formula@2x.png" directory))
+         (org-preview-latex-process-alist
+          '((test :programs nil
+                  :image-input-type "pdf"
+                  :image-output-type "png"
+                  :latex-header ""
+                  :latex-compiler ("latex")
+                  :image-converter ("convert")
+                  :post-clean (".tex" ".pdf" ".png"))))
+         converter-specs)
+    (unwind-protect
+        (cl-letf (((symbol-function 'org-latex-color-format)
+                   (lambda (_color) "0,0,0"))
+                  ((symbol-function 'org-compile-file)
+                   (lambda (source _process extension
+                                   &optional _err-msg _log-buf spec)
+                     (when spec (push spec converter-specs))
+                     (let ((output (concat (file-name-sans-extension source)
+                                           "." extension)))
+                       (with-temp-file output
+                         (insert (if spec (alist-get ?D spec) "input")))
+                       output))))
+          (org-create-formula-image
+           "x" tofile (list :html-scale 1.0 :to2xfile to2xfile) nil 'test)
+          (should (= (length converter-specs) 2))
+          (let* ((dpi (lambda (spec) (string-to-number (alist-get ?D spec))))
+                 (specs (sort converter-specs
+                              (lambda (a b) (< (funcall dpi a)
+                                               (funcall dpi b)))))
+                 (spec-1x (car specs))
+                 (spec-2x (cadr specs)))
+            (should (= (funcall dpi spec-2x) (* 2 (funcall dpi spec-1x))))
+            (should (equal (alist-get ?S spec-2x)
+                           (alist-get ?S spec-1x)))
+            (should (equal (with-temp-buffer
+                             (insert-file-contents tofile)
+                             (buffer-string))
+                           (alist-get ?D spec-1x)))
+            (should (equal (with-temp-buffer
+                             (insert-file-contents to2xfile)
+                             (buffer-string))
+                           (alist-get ?D spec-2x)))))
+      (delete-directory directory t))))
 
 ;;; org-tests.el ends here
